fc1b26301d2db3076fe2f832ae029b749a272c34
Gasimov Markaz <142661247+mrkzqsmv@users.noreply.github.com>
2024-02-23 11:05:40 +0400

    regexTasks2withTypesctipt

---
+++ regexTask2withTypescript.js
@@ -0,0 +1,75 @@
+//PHONE NUMBER VALIDATION WITH REGEX
+// const phoneNumberRegex:RegExp = /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/g;
+// const phoneNum:string = '+994503575344';
+
+// const validateUserPhoneNumber:Function = (phoneNum:string) => {
+//     if (phoneNum.match(phoneNumberRegex)) {
+//         return 'TRUE PHONE NUMBER';
+//     } else {
+//         return 'FALSE PHONE NUMBER';
+//     }
+// }
+// const phoneNumber:string = validateUserPhoneNumber(phoneNum);
+console.log(phoneNumber);
+
+
+//EMAIL
+// const emailRegex:RegExp = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+// const emailAddress:string = 'markazgasimov@gmail.com';
+
+// const validateUserEmailAddress:Function = (emailAddress:string) => {
+//     if (emailAddress.match(emailRegex)) {
+//         return 'TRUE EMAIL ADDRESS';
+//     } else {
+//         return 'FALSE EMAIL ADDRESS';
+//     }
+// }
+
+// const email:string = validateUserEmailAddress(emailAddress);
+
+console.log(email)
+
+
+
+
+//PASSWORD
+// const passwordRegex:RegExp = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm;
+// const userPassword:string = 'Markaz1234567.';
+
+// const validateUserPassword:Function = (userPassword:string) => {
+//     if (userPassword.match(passwordRegex)) {
+//         return 'TRUE PASSWORD';
+//     } else {
+//         return 'FALSE PASSWORD';
+//     }
+// }
+// const password:string = validateUserPassword(userPassword);
+console.log(password);
+
+
+//NAME
+// const nameRegex:RegExp = /\b([A-ZÀ-ÿ][-,a-z. ']+[ ]*)+/gm;
+// const userName:String = 'Markaz';
+
+// const validateUserName:Function = (userName:string) => {
+//     if (userName.match(nameRegex)) {
+//         return 'TRUE USERNAME';
+//     } else {
+//         return 'FALSE USERNAME';
+//     }
+// }
+// const username:string = validateUserName(userName);
+console.log(username);
+
+
+//AGE
+// const ageRegex:RegExp = /^(1[89]|[2-9]\d)$/gm;
+// const validateAge:Function = (age:string) => {
+//     if (age.match(ageRegex)) {
+//         return 'TRUE AGE';
+//     } else {
+//         return 'FALSE AGE';
+//     }
+// }
+// const age:string = validateAge('20');
+console.log(age);
