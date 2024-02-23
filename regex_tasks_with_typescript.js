ef8cd9659f1e7d25cb7bed51965709b52190b248
Gasimov Markaz <142661247+mrkzqsmv@users.noreply.github.com>
2024-02-23 10:58:15 +0400

    regex tasks with typecript

---
+++ regex_tasks_with_typescript.js
@@ -0,0 +1,38 @@
+//TASK 1
+// const regexText:RegExp = /Spain/g;
+
+// const findedWords: Array<String> = text.match(regexText);
+
+// console.log(findedWords);
+
+//TASK 2
+
+// const searchText: RegExp = /rain/g;
+
+// const searchTexts = text.matchAll(searchText);
+
+// for (const searchText of searchTexts) {
+//     console.log(searchText);
+// }
+
+
+//TASK 3
+
+// const modifiedText:string = text.replace('Spain', 'France');
+
+// console.log(modifiedText);
+
+//TASK 4
+
+// const modifiedText2:string = text.replaceAll('rain', 'sun');
+
+// console.log(modifiedText2);
+
+
+//TASK 5
+
+// const searchText2:RegExp= /plain/g;
+
+// const findedWords2:Number = text.search(searchText2);
+
+// console.log(findedWords2);
